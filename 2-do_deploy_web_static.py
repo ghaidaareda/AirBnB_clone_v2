716693de13c0c8ee26de22e132fd90fd06b6609f
ghaidaareda <dodo.sa7afy@gmail.com>
2023-10-06 04:52:52 +0300

    2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,6 +6,7 @@ from the contents of the web_static folder
 import os
 from datetime import datetime
 from fabric.api import local
+from fabric import Connection
 
 
 def do_pack():
@@ -22,3 +23,16 @@ def do_pack():
         return f"versions/{arc_name} web_static"
     except Exception:
         return None
+    
+def do_deploy(archive_path):
+    if archive_path:
+        env.hosts = ['<IP web-01>', '<IP web-02>']
+        c = Connection(host=env.hosts, user='<username>', connect_kwargs={'key_filename': '/path/to/ssh/key'})
+        remote = '/tmp/{}'.format(os.path.basename(archive_path))
+        c.put(archive_path, remote)
+        remote_dir = '/data/web_static/releases/{}'.format(os.path.splitext(os.path.basename(archive_path))[0])
+        c.run('sudo mkdir -p {}'.format(remote_dir))
+        c.run('sudo tar -xzf {} -C {} --strip-components=1'.format(remote, remote_dir))
+        c.run('sudo service nginx restart')
+    else:
+        return False
