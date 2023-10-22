b47494595c72192309bb7650ff9cff7e995a0135
ghaidaareda <dodo.sa7afy@gmail.com>
2023-10-22 23:30:21 +0300

    2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+script that starts a Flask web application with added route
+"""
+from flask import Flask
+
+my_app = Flask(__name__)
+
+
+@my_app.route("/", strict_slashes=False)
+def homepage():
+    """home_page"""
+    return "Hello HBNB!"
+
+
+@my_app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """hbnb_page"""
+    return "HBNB"
+
+@my_app.route("/c/<text>", strict_slashes=False)
+def variable(text):
+    """text_page"""
+    return "C {}".format(text.replace('_', ' '))
+
+
+if __name__ == "__main__":
+    my_app.run(host='0.0.0.0', port=5000, debug=True)
