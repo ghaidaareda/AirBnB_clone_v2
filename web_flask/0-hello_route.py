da00a2050e6b69ccff10e053f71064efced18188
ghaidaareda <dodo.sa7afy@gmail.com>
2023-10-22 20:31:20 +0300

    0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,6 +8,7 @@ my_app = Flask(__name__)
 
 @my_app.route("/", strict_slashes=False)
 def homepage():
+    """home_page"""
     return "Hello HBNB!"
 if __name__ == "__main__":
     my_app.run(host = '0.0.0.0', port=5000, debug=True)
