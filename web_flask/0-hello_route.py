7bb82358fe5411548e4b762ec591cd348bd2c314
ghaidaareda <dodo.sa7afy@gmail.com>
2023-10-22 20:34:19 +0300

    pycode

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,10 +6,12 @@ from flask import Flask
 
 my_app = Flask(__name__)
 
+
 @my_app.route("/", strict_slashes=False)
 def homepage():
     """home_page"""
     return "Hello HBNB!"
+
+
 if __name__ == "__main__":
-    my_app.run(host = '0.0.0.0', port=5000, debug=True)
-    
+    my_app.run(host='0.0.0.0', port=5000, debug=True)
