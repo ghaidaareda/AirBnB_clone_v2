db8f7699b5791571bc0c0f92099a98d71dc96656
ghaidaareda <dodo.sa7afy@gmail.com>
2023-10-26 00:39:36 +0300

    81

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,7 +4,7 @@ script that starts a Flask web application with added route
 """
 from flask import Flask, render_template
 from models import storage
-
+from models.state import State
 
 app = Flask(__name__)
 
